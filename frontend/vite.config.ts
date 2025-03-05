0b8a18b5f4236b426d97ade6c987f7e4cc5471a9
valgardg <valgardo20@ru.is>
2025-03-05 00:38:37 +0000

    added: raspi.valgard.org to allowed hosts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,6 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    allowedHosts: ['raspberrypi.local'],
+    allowedHosts: ['raspberrypi.local', 'raspi.valgard.org'],
   },
 })
